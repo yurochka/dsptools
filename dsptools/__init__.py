582a6e221679775928cfec44750e2b66e1ea1507
yurochka <swissslove@gmail.com>
2018-06-28 18:59:47 +0800

    Update __init__.py

--- dsptools/__init__.py
+++ dsptools/__init__.py
@@ -1,5 +1,6 @@
 from .d2d import d2d
 from .invfreqz import invfreqz
 from .polystab import polystab
+from .tfminphase import tfminphase
 
-__all__ = ['d2d', 'invfreqz', 'polystab']
+__all__ = ['d2d', 'invfreqz', 'polystab', 'tfminphase']
